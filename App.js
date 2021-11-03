6aabc3f345a3f3ae336f361e31c84d436cfbac44
Gustavo <60441031+Gustavox98@users.noreply.github.com>
2021-11-02 22:33:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './GustavoMontano.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My name is Gustavo Montano and I'm going to master React!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Gustavox98"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check Me Out!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
